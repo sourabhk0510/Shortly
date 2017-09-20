01d89a93ec995317af4d4ac1aa939bbf1319e8d9
sourabhk0510 <sourabhk0510@gmail.com>
2017-09-21 02:11:06 +0530

    unused code removed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-  check_authorization unless: :devise_controller?
+  # check_authorization unless: :devise_controller?
   protect_from_forgery with: :exception
-    rescue_from CanCan::AccessDenied do |exception|
-    render :file => "#{Rails.root}/public/403.html", :status => 403, :layout => false
-    ## to avoid deprecation warnings with Rails 3.2.x (and incidentally using Ruby 1.9.3 hash syntax)
-    ## this render call should be:
-    # render file: "#{Rails.root}/public/403", formats: [:html], status: 403, layout: false
-  end
 end
