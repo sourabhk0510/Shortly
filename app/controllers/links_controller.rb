426520988a42368c86f91924aa54425c78d1cb0c
sourabhk0510 <sourabhk0510@gmail.com>
2017-09-21 03:30:34 +0530

    short link create done

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,18 +1,16 @@
 class LinksController < ApplicationController
+  
   def index
-    
-  end
-
-  def new
-    
   end
 
   def create
-    
+    @link = Link.create(base_url: link_params[:base_url], short_url: SecureRandom.hex(4))
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'pages#home'
-  resources :links, only: [ :index, :new, :create, :destroy]
+  resources :links, only: [ :index, :create, :destroy]
 end
