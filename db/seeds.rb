6d944621bdf6e819f752cb7844819c1eb958ad4e
sourabhk0510 <sourabhk0510@gmail.com>
2017-09-22 02:42:37 +0530

    seed file changed new links added to it

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,20 @@
 #   
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-puts "Creating admin"
-User.create(email: "user@email.com", password: "password", password_confirmation: "password")
+puts "Creating admin..."
+user = User.create(email: "user@email.com", password: "password", password_confirmation: "password", role: "admin")
+puts "Admin Created!"
+puts "Creating links..."
+Link.create(base_url: "https://www.google.com", short_url: SecureRandom.hex(3), clicks: 100000, creator_id: user.id)
+Link.create(base_url: "https://www.youtube.com", short_url: SecureRandom.hex(3), clicks: 995000, creator_id: user.id)
+Link.create(base_url: "https://www.facebook.com", short_url: SecureRandom.hex(3), clicks: 920000, creator_id: user.id)
+Link.create(base_url: "https://www.baidu.com", short_url: SecureRandom.hex(3), clicks: 870010, creator_id: user.id)
+Link.create(base_url: "https://www.wikipedia.org", short_url: SecureRandom.hex(3), clicks: 870000, creator_id: user.id)
+Link.create(base_url: "https://www.yahoo.com", short_url: SecureRandom.hex(3), clicks: 854600, creator_id: user.id)
+Link.create(base_url: "https://www.google.co.in", short_url: SecureRandom.hex(3), clicks: 843870, creator_id: user.id)
+Link.create(base_url: "https://www.amazon.com", short_url: SecureRandom.hex(3), clicks: 826770, creator_id: user.id)
+Link.create(base_url: "https://www.twitter.com", short_url: SecureRandom.hex(3), clicks: 822220, creator_id: user.id)
+Link.create(base_url: "https://www.live.com", short_url: SecureRandom.hex(3), clicks: 812230, creator_id: user.id)
+Link.create(base_url: "https://www.instagram.com", short_url: SecureRandom.hex(3), clicks: 812980, creator_id: user.id)
+puts "Done creaing links!"
+puts "Seed Complete!"
